206d38acdba307bf80a0e003306eaa042de6cde7
Antony2811 <118750593+Antony2811@users.noreply.github.com>
2023-03-21 17:44:00 -0400

    modificacion 21-3-2023

--- app.py
+++ app.py
@@ -1,12 +1,30 @@
-from flask import Flask
-from time1 import get_time
+from flask import Flask, request
+from time1 import get_time1
 from date import get_date
 
 app = Flask(__name__)
 
-@app.route('/time')
-def time():
-    return get_time()
+@app.route('/api/time', methods=['GET']) 
+
+def get_time(): 
+
+    api_type = request.args.get('tipo_api', 'hour') # Get the requested API type, default to 'hour' 
+
+    if api_type == 'hour': 
+
+        return get_time1() # Respond with the current hour in 24-hour format 
+
+    elif api_type == 'date': 
+
+        return get_date() # Respond with the current datetime in YYYY-MM-DD HH:MM:SS format 
+    
+    elif api_type == "mensaje":
+
+        return "Hello Omar!"
+
+    else: 
+
+        return 'Invalid API type requested. Valid types are "hour" and "datetime"'
 
 @app.route('/date')
 def date():
--- time1.py
+++ time1.py
@@ -1,5 +1,5 @@
 import datetime
 
-def get_time():
+def get_time1():
     now = datetime.datetime.now()
     return now.strftime("%H:%M:%S")
